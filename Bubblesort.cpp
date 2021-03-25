c7b70790be3201d32899f102c6d184a569db9b41
mohammad kaif <mohammadkaif051197@gmail.com>
2021-03-25 12:57:39 +0530

    Yes me

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -27,7 +27,7 @@ void bubblesort(int *s, int n)
 int main()
 {
     // int arr[] = {3, 5346, 3462, 5, 73, 99, 10, 84, 1};
-    int arr[] = {1, 2, 3, 3, 56, 89, 5, 6, 7};
+    int arr[] = {56,3, 3,89,789,90,2341, 2,43,87,23,632,85,32,56, 5, 6, 7};
     int size = sizeof(arr) / sizeof(int);
     bubblesort(arr, size);
     for (int i = 0; i < size; i++)
---
+++ Insertionsort.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void insertionSort(int * s,int size){
+    for (int  i = 0; i < size-1; i++)
+    {
+        
+    }
+    
+}
+int main(){
+    int arr[] = {56,3, 3,89,789,90,2341,2,43,87,23,632,85,32,56, 5, 6, 7};
+    int size = sizeof(arr)/sizeof(int);
+    insertionSort(arr,size);
+    return 0;
+}
